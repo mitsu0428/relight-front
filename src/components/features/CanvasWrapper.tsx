83d88c6f3fb029080b30f73d6a84c21f6acd407a
mitsu0428 <mitsuprograming@gmail.com>
2023-12-07 19:04:34 +0900

    fix: 謎の色無くす

--- src/components/features/CanvasWrapper.tsx
+++ src/components/features/CanvasWrapper.tsx
@@ -1,27 +1,10 @@
 import React, { useRef, useState } from "react";
 import * as CanvasWrapper from "@/components/designSystems/CanvasWrapper";
 import { Canvas, extend, useLoader } from "@react-three/fiber";
-import {
-  OrbitControls,
-  Environment,
-  Effects,
-  useTexture,
-} from "@react-three/drei";
-
-import { LUTPass, LUTCubeLoader } from "three-stdlib";
+import { OrbitControls, Environment } from "@react-three/drei";
+import { LUTPass } from "three-stdlib";
 
 extend({ LUTPass });
-
-function Grading() {
-  return (
-    <Effects>
-      {/* <lUTPass
-        lut={texture3D}
-        intensity={0.75}
-      /> */}
-    </Effects>
-  );
-}
 
 function Sphere(props: JSX.IntrinsicElements["mesh"]) {
   return (
@@ -52,12 +35,6 @@ export const Components = () => {
           position={[5, 15, 10]}
         />
         <Sphere />
-        <Grading />
-        <Environment
-          preset="dawn"
-          background
-          blur={0.6}
-        />
         <OrbitControls />
       </Canvas>
     </CanvasWrapper.Components>
