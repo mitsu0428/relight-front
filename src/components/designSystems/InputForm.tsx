a12b243ec16b4645aef087afe6dff801efad9a22
mitsu0428 <mitsuprograming@gmail.com>
2023-12-08 13:17:05 +0900

    fix: inputformの判定ロジック

--- src/components/designSystems/InputForm.tsx
+++ src/components/designSystems/InputForm.tsx
@@ -7,6 +7,13 @@ type Props = {
 };
 
 export const Components = ({ placeholder, className }: Props) => {
+  const [state, setState] = React.useState("close");
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setState(e.target.value);
+  };
+
+  const password = "xxx";
+
   return (
     <span
       style={{
@@ -21,6 +28,20 @@ export const Components = ({ placeholder, className }: Props) => {
         type="text"
         placeholder={placeholder}
         className={className}
+        onChange={handleChangeInput}
+      />
+      <button
+        style={{
+          margin: "10px",
+          padding: "10px",
+        }}
+        onClick={() => {
+          if (state === password) {
+            window.location.href = "/nextPage";
+          } else {
+            alert("Not Matched");
+          }
+        }}
       />
       <PiLockKey />
     </span>
